e16c282083de9f7b42607db531b9cd46ddd7586f
RashadArbab <rashad.arbab@gmail.com>
2020-10-28 12:09:33 -0400

    aws setup

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.get('/api/homePage', (req , res)=>{
 
 
 
-app.listen(8080 , ()=>{
-    console.log('server live'); 
-}); 
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+const server = app.listen(port, function () {
+    console.log('Server listening on port ' + port);
+});
